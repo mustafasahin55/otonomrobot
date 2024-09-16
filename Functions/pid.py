dac6143966c0bc3609815898d3ab14daab945109
Yunus <117470768+Yunsst@users.noreply.github.com>
2024-09-16 11:21:41 +0300

    Add files via upload

---
+++ Functions/pid.py
@@ -0,0 +1,49 @@
+import time
+
+class PID:
+    def __init__(self, P=1, I=0.0, D=0.0):
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
+        self.sample_time = 0.00
+        self.current_time = time.time()
+        self.last_time = self.current_time
+        self.clear()
+
+    def clear(self):
+        self.SetPoint = 0.0
+        self.PTerm = 0.0
+        self.ITerm = 0.0
+        self.DTerm = 0.0
+        self.last_error = 0.0
+
+    def update(self, feedback_value):
+        error = self.SetPoint - feedback_value
+        self.current_time = time.time()
+        delta_time = self.current_time - self.last_time
+        delta_error = error - self.last_error
+
+        if (delta_time >= self.sample_time):
+            self.PTerm = self.Kp * error
+            self.ITerm += error * delta_time
+            self.DTerm = 0.0
+            if delta_time > 0:
+                self.DTerm = delta_error / delta_time
+
+            self.last_time = self.current_time
+            self.last_error = error
+
+            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+
+    def setKp(self, proportional_gain):
+        self.Kp = proportional_gain
+
+    def setKi(self, integral_gain):
+        self.Ki = integral_gain
+
+    def setKd(self, derivative_gain):
+        self.Kd = derivative_gain
+
+    def setSampleTime(self, sample_time):
+        self.sample_time = sample_time
+
